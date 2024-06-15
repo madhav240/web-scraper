c6892e5e97ecabdc9bc0103dc08c5e2843607623
Madhav Mishra <60259965+madhav240@users.noreply.github.com>
2024-06-15 20:15:59 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -173,7 +173,7 @@ export default function Home() {
   const [loading, setLoading] = useState(false);
   const [totalRows, setTotalRows] = useState(0);
   const [page, setPage] = useState(1);
-  const limit = 10;
+  const limit = 5;
   const totalPages = Math.ceil(totalRows / limit);
 
   function convertArrayOfObjectsToCSV(array: []) {
